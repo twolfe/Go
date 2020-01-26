301b16ec72aab6720dfecb17ae04115dea601a09
twolfe <thomsa.wolfe@univie.ac.at>
2020-01-26 20:19:53 +0100

    jan2020

--- causality/causality-func.go
+++ causality/causality-func.go
@@ -23,7 +23,7 @@ import (
    ##       ##    ##        ########  ######
 */
 
-type F func(out chan<- float64, in ...<-chan float64)
+type F func(out chan<- float64, in <-chan float64)
 
 /*
 	 ######   #######  ##     ## ##    ## ######## ######## ########
@@ -146,10 +146,7 @@ func printer(in <-chan float64) {
 func main() {
 	in := make(chan float64)
 	//naturals2 := make(chan float64)
-	out1 := make(chan float64)
-	out2 := make(chan float64)
-	out3 := make(chan float64)
-	out4 := make(chan float64)
+	out := make(chan float64)
 	//squares2 := make(chan float64)
 	//quads := make(chan float64)
 	//var chansIn ch //[1]ch
@@ -160,64 +157,26 @@ func main() {
 	/*for i := range chans {
 		 chans[i] = make(ch)
 	}*/
-	//var n1 F
-	n1 := func(out chan<- float64, in <-chan float64) {
-		dist := distuv.Normal{
-			Mu:    9,
-			Sigma: 1,
-			//Rate: 0.01,
-		}
-		for x := 0; x <= 100; x++ {
-			out <- dist.Rand()
-		}
-		close(out)
-	}
-
-	n2 := func(out chan<- float64, in <-chan float64) {
-		dist := distuv.Normal{
-			Mu:    1,
-			Sigma: 1,
-		}
-		for v := range in {
-			out <- v + dist.Rand()
-		}
-		close(out)
-	}
-
-	n3 := func(out chan<- float64, in <-chan float64) {
-		dist := distuv.Normal{
-			Mu:    1,
-			Sigma: 1,
-		}
-		for v := range in {
-			out <- v + dist.Rand()
-		}
-		close(out)
-	}
-
-	n4 := func(out chan<- float64, in1 <-chan float64, in2 <-chan float64) {
-		dist := distuv.Normal{
-			Mu:    1,
-			Sigma: 1,
-		}
-		for v1 := range in1 {
-			for v2 := range in2 {
-				out <- v1/v2 + dist.Rand()
+	node := Node{
+		input:    in,
+		output:   out,
+		parents:  nil,
+		children: nil,
+		function: func(out chan<- float64, in <-chan float64) {
+			for v := range in {
+				out <- v / v
 			}
 			close(out)
-		}
+		},
 	}
 
-	go n1(out1, in)
-	go n2(out2, out1)
-	go n3(out3, out1)
-	go n4(out4, out2, out3)
-	//go counter(in)
+	go counter(in)
 	//go counter(naturals2)
 	//go squarer1(squares1, naturals1)
 	//go squarer2(squares2, naturals1)
 	//go quad(quads, squares1, squares2)
 	//v := node.function
+	go node.function(node.output, node.input)
 	//fmt.Println(v)
-	printer(out4)
+	printer(node.output)
 }
