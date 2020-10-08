199dd1a47177c227469dd6bae0939e07a4ec8e99
twolfe <thomas.m.wolfe@gmail.com>
2020-10-08 16:58:16 +0200

    hiding-in-plain-site

--- modelling/wolbachia-dynamics/hiding-in-plain-site.go
+++ modelling/wolbachia-dynamics/hiding-in-plain-site.go
@@ -8,27 +8,51 @@ import (
 	//"strings"
 )
 
+type Organism interface {
+	Children() bool
+}
+
 type Host struct {
-	sex      string
+	sex      bool
 	infected Wolbachia
 	resitant bool
+}
+
+func (h Host) Children() bool {
+	return h.resitant
 }
 
 type Wolbachia struct {
 	ci bool
 }
 
+func (w Wolbachia) Children() bool {
+	return w.ci
+}
+
+func replicate(o Organism) {
+	fmt.Println(o)
+	fmt.Println(o.Children())
+}
+
+func migrate(h Host) {
+	fmt.Println(h)
+}
+
 func main() {
 	jane := Wolbachia{
 		ci: true,
 	}
+	//jane := o
 	harry := Host{
-		sex:      "male",
+		sex:      false,
 		infected: jane,
 		resitant: false,
 	}
 
-	fmt.Println(harry)
+	replicate(harry)
+	replicate(jane)
+	migrate(harry)
 }
 
 //interface mutate
